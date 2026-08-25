abbecf0ade6312a309ac934679330cbe2928cad3
CurcoVain <gg.prof.ef@gmail.com>
2026-08-25 20:50:54 -0300

    Estructura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, CategoriesModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 })
 export class AppModule {}
