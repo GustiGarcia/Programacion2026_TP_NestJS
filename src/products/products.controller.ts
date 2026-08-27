5f9be85f4820850431eadc2ced663e07923964be
GustiGarcia <gg.prof.ef@gmail.com>
2026-08-26 23:23:00 -0300

    Filter products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,13 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    ParseIntPipe,
-    Post,
-    Put,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -14,33 +15,36 @@ import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productService: ProductsService) {}
+  constructor(private readonly productService: ProductsService) {}
 
-    @Post()
-    create(@Body() createProductDto: CreateProductDto) {
-        return this.productService.create(createProductDto);
-    }
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
 
-    @Get()
-    findAll() {
-        return this.productService.findAll();
-    }
+  @Get()
+  findAll(
+    @Query('minPrice', new ParseIntPipe({ optional: true })) minPrice?: number,
+    @Query('maxPrice', new ParseIntPipe({ optional: true })) maxPrice?: number,
+  ) {
+    return this.productService.findAll(minPrice, maxPrice);
+  }
 
-    @Get(':id')
-    findOne(@Param('id', ParseIntPipe) id: number) {
-        return this.productService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.findOne(id);
+  }
 
-    @Put(':id')
-    update(
-        @Param('id', ParseIntPipe) id: number,
-        @Body() updateProductDto: UpdateProductDto,
-    ) {
-        return this.productService.update(id, updateProductDto);
-    }
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productService.update(id, updateProductDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id', ParseIntPipe) id: number) {
-        return this.productService.remove(id);
-    }
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.remove(id);
+  }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -5,50 +5,54 @@ import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-    private products: Product[] = [];
-    private nextId = 1;
+  private products: Product[] = [];
+  private nextId = 1;
 
-    create(dto: CreateProductDto) {
-        const product: Product = {
-            id: this.nextId++,
-            ...dto,
-            type: dto.type ?? 'physical',
-        };
-        this.products.push(product);
-        return product;
+  create(dto: CreateProductDto) {
+    const product: Product = {
+      id: this.nextId++,
+      ...dto,
+      type: dto.type ?? 'physical',
+    };
+    this.products.push(product);
+    return product;
+  }
+
+  findAll(minPrice?: number, maxPrice?: number) {
+    return this.products.filter((p) => {
+      const pasaMinimo = minPrice === undefined || p.price >= minPrice;
+      const pasaMaximo = maxPrice === undefined || p.price <= maxPrice;
+      return pasaMinimo && pasaMaximo;
+    });
+  }
+
+  findOne(id: number) {
+    const product = this.products.find((p) => p.id === id);
+
+    if (!product) {
+      throw new NotFoundException(`Producto con id ${id} no encontrado`);
     }
 
-    findAll() {
-        return this.products;
+    return product;
+  }
+
+  update(id: number, dto: UpdateProductDto) {
+    const product = this.findOne(id);
+
+    Object.assign(product, dto);
+
+    return product;
+  }
+
+  remove(id: number) {
+    const index = this.products.findIndex((p) => p.id === id);
+
+    if (index === -1) {
+      throw new NotFoundException(`Producto con id ${id} no encontrado`);
     }
 
-    findOne(id: number) {
-        const product = this.products.find((p) => p.id === id);
+    const [deleted] = this.products.splice(index, 1);
 
-        if (!product) {
-            throw new NotFoundException(`Producto con id ${id} no encontrado`);
-        }
-
-        return product;
-    }
-
-    update(id: number, dto: UpdateProductDto) {
-        const product = this.findOne(id);
-
-        Object.assign(product, dto);
-
-        return product;
-    }
-
-    remove(id: number) {
-        const index = this.products.findIndex((p) => p.id === id);
-
-        if (index === -1) {
-            throw new NotFoundException(`Producto con id ${id} no encontrado`);
-        }
-
-        const [deleted] = this.products.splice(index, 1);
-
-        return deleted;
-    }
+    return deleted;
+  }
 }
